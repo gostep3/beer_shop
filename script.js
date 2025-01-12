4217d90ed27e629ba145700778b7ece1c7530ed5
gostep3 <153300575+gostep3@users.noreply.github.com>
2025-01-12 18:02:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 "use script"
 const main = document.querySelector("main")
 const theme = document.querySelector("#checkbox")
-const btn = document.querySelectorAll("#btn-kors")
-const btnMain = document.querySelector("#btn-main")
-const mainKors = document.querySelector("#main-kors")
+const btns = document.querySelectorAll("btn-kors")
 const korsine = document.querySelector("#korsine")
+const parentKors = document.querySelector("#cors")
 let kors = [];
+setInterval(function(){
+    alert("Вы еще здесь? Закажите что-нибудь <3")
+}, 500000)
+
+for (let i = 0; i < btns.length; i++) {
+    
+    btns[i].addEventListener("click", function(){
+        console.log(1)
+        kors.push(btns[i].parentElement.innerHTML)
+    })
+}
 
 theme.addEventListener("click", function() {
     main.classList.toggle("dark-theme");
     main.classList.toggle("light-theme");
 })
 
-
-
-// btnMain.addEventListener("click", function(){
-//     console.log(1)
-//     main.style.display = "0";
-//     mainKors.style.display = "none";
-// })
-
-// korsine.addEventListener("click", function(){
-//     main.style.display = "none";
-//     mainKors.style.display = "0";
-//     for (let i = 0; i < kors.length; i++){
-//         document.body.createElement(kors[i]);
-//     }
-// })
+korsine.addEventListener("click", function(){
+    main.classList.toggle("kors-theme")
+    for (let i = 0; i < kors.length; i++){
+        let newElem = document.createElement("DIV")
+        newElem.innerHTML = kors[i]
+        document.body.appendChild(newElem)
+    }
+})
